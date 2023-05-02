c65ebe4af0414dc33ef38024345a7e355884bce5
Siddharth Kundu <sk31395@gmail.com>
2023-05-03 02:21:15 +0530

    basic usage statistic setup

---
+++ src/index.mjs
@@ -0,0 +1,26 @@
+import { h, render } from 'https://esm.sh/preact';
+import htm from 'https://esm.sh/htm';
+
+// Initialize htm with Preact
+const html = htm.bind(h);
+
+function App(props) {
+  return html`
+    <div>
+      ${props.cpus.map((cpu) => {
+    return html`<div>${cpu.toFixed(2)}% usage</div>`;
+  })
+    }
+    </div>
+  `;
+}
+
+setInterval(async () => {
+  let response = await fetch("/api/cpus");
+  if (response.status !== 200) {
+    throw new Error(`HTTP error! Status: ${response.status} `);
+  }
+
+  let json = await response.json();
+  render(html`<${App} cpus=${json} />`, document.body);
+}, 1000);
